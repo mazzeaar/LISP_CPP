6120a697a57e546e4159abb49395616d8a2e3baa
amazzetta <aaron.mazzetta@gmail.com>
2024-01-30 14:33:35 +0100

    removed namespace

--- impls/cpp/include/type_base.h
+++ impls/cpp/include/type_base.h
@@ -5,88 +5,80 @@
 #include <memory>
 #include <string>
 
-namespace types {
 
-    class ReferenceCounter {
-    public:
-        ReferenceCounter() : m_count(size_t(0)) { }
+class ReferenceCounter {
+public:
+    ReferenceCounter() : m_count(size_t(0)) { }
 
-        ReferenceCounter(const ReferenceCounter&) = delete;
-        ReferenceCounter& operator=(const ReferenceCounter&) = delete;
+    ReferenceCounter(const ReferenceCounter&) = delete;
+    ReferenceCounter& operator=(const ReferenceCounter&) = delete;
 
-        virtual ~ReferenceCounter() { }
+    virtual ~ReferenceCounter() { }
 
-        const ReferenceCounter* acquire() const { m_count++; return this; }
-        size_t release() const { return --m_count; }
-        size_t count() const { return m_count; }
+    const ReferenceCounter* acquire() const { m_count++; return this; }
+    size_t release() const { return --m_count; }
+    size_t count() const { return m_count; }
 
-    private:
-        mutable size_t m_count;
-    };
+private:
+    mutable size_t m_count;
+};
 
-    /**
-     * template<typename T>
-     * concept IsRefCounterDerived = std::is_base_of<RefCounter, T>::value;
-     * requires IsRefCounterDerived<T>
-    */
+template<class T>
+class RefCountedPtr {
+public:
+    RefCountedPtr() : m_object(0) { }
 
-    template<class T>
-    class RefCountedPtr {
-    public:
-        RefCountedPtr() : m_object(0) { }
+    RefCountedPtr(T* object) : m_object(0)
+    {
+        acquire(object);
+    }
 
-        RefCountedPtr(T* object) : m_object(0)
-        {
-            acquire(object);
+    RefCountedPtr(const RefCountedPtr& rhs) : m_object(0)
+    {
+        acquire(rhs.m_object);
+    }
+
+    ~RefCountedPtr() { release(); }
+
+    const RefCountedPtr& operator=(const RefCountedPtr& rhs)
+    {
+        acquire(rhs.m_object);
+        return *this;
+    }
+
+    bool operator == (const RefCountedPtr& rhs) const { return m_object == rhs.m_object; }
+    bool operator != (const RefCountedPtr& rhs) const { return m_object != rhs.m_object; }
+
+    operator bool() const { return m_object != NULL; }
+
+    T* operator -> () const { return m_object; }
+    T* ptr() const { return m_object; }
+
+    friend std::ostream& operator<<(std::ostream& os, const RefCountedPtr& ptr)
+    {
+        os << ptr->toString();
+        return os;
+    }
+
+private:
+    void acquire(T* object)
+    {
+        if ( object != NULL ) {
+            object->acquire();
         }
 
-        RefCountedPtr(const RefCountedPtr& rhs) : m_object(0)
-        {
-            acquire(rhs.m_object);
+        release();
+        m_object = object;
+    }
+
+    void release()
+    {
+        if ( (m_object != NULL) && (m_object->release() == 0) ) {
+            delete m_object;
         }
+    }
 
-        ~RefCountedPtr() { release(); }
-
-        const RefCountedPtr& operator=(const RefCountedPtr& rhs)
-        {
-            acquire(rhs.m_object);
-            return *this;
-        }
-
-        bool operator == (const RefCountedPtr& rhs) const { return m_object == rhs.m_object; }
-        bool operator != (const RefCountedPtr& rhs) const { return m_object != rhs.m_object; }
-
-        operator bool() const { return m_object != NULL; }
-
-        T* operator -> () const { return m_object; }
-        T* ptr() const { return m_object; }
-
-        friend std::ostream& operator<<(std::ostream& os, const RefCountedPtr& ptr)
-        {
-            os << ptr->toString();
-            return os;
-        }
-
-    private:
-        void acquire(T* object)
-        {
-            if ( object != NULL ) {
-                object->acquire();
-            }
-
-            release();
-            m_object = object;
-        }
-
-        void release()
-        {
-            if ( (m_object != NULL) && (m_object->release() == 0) ) {
-                delete m_object;
-            }
-        }
-
-        T* m_object;
-    };
-} // namespace types
+    T* m_object;
+};
 
 #endif // TYPE_BASE_H
