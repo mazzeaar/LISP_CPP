4df4093319e43f2b5794195ff72457ee27345bab
aaron <74001922+mazzeaar@users.noreply.github.com>
2024-02-21 11:46:34 +0100

    Delete impls/aaron directory

--- impls/aaron/include/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-char unescape(char c);
-inline std::string escape(char c);
-const std::string unescape(const std::string& in);
-const std::string escape(const std::string& in);
-
-#endif // UTILS_H
--- impls/aaron/steps/step4_if_fn_do.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "NOT IMPLEMENTED!\n";
-}
